5c69253f351b94b2882081b3db5fdc3c4de9a1fa
Will Dixon <dixonwille@hotmail.com>
2018-02-11 15:50:45 -0500

    Change out the thermometer

--- 2018PowerUpSensors.ino
+++ 2018PowerUpSensors.ino
@@ -1,20 +1,20 @@
-#include <thermistor.h>
 #include <hcsr04.h>
 #include <Wire.h>
 
 // Ultrasonic setup
-const int numUltras = 5;
+const int numUltras = 4;
 HCSR04 ultrasonics[numUltras] = {
   {7, 2},
   {8, 3},
   {9, 4},
-  {10, 5},
-  {11, 6}
+  {10, 5}
+  //{11, 6}
 };
 int distances[numUltras];
 
 //Temperature setup
-Thermistor thermistor = Thermistor::FromCelsius(A0, 10000, 25, 4300, 98800);
+const int thermPin = A0;
+//Thermistor thermistor = Thermistor::FromCelsius(A0, 10000, 25, 4300, 98800);
 
 //Setup I2C
 const int SLAVE_ADDRESS = 0x2A;
@@ -29,14 +29,14 @@ void setup() {
 }
 
 void loop() {
-  float temp = thermistor.temperatureC();
+  float temp = temperatureC();
   Serial.print(temp);
   Serial.print("; ");
   float speedOfSound = (331.3 + 0.606 * temp) / 10000; // Calculate the speed of sound using temperature in cm/us
 
   // Go through Round Robin and calculate the distance
   for (int i = 0; i < numUltras; i++) {
-    delayMicroseconds(10); // High signals must be greater than 10 microseconds
+    delayMicroseconds(100); // High signals must be greater than 10 microseconds
     distances[i] = ultrasonics[i].distance(speedOfSound);
     Serial.print(i);
     Serial.print(":");
@@ -72,5 +72,12 @@ void storeData(){
       arrayindex++;
     }
   }
+}
+
+float temperatureC() {
+  int tempReading = analogRead(thermPin);
+  float voltage = tempReading * 3.23;
+  voltage /= 1024.0;
+  return (voltage - 0.5) * 100.0;
 }
 
